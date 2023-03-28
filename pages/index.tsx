f90008564d7823068342649a433c972f928dd731
spreadmycode <spreadmycode@outlook.com>
2023-03-27 18:06:51 -0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Youthfully Imgur</title>
+        <meta name="description" content="Youthfully Imgur" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className=""></main>
+    </>
+  );
+}
